6e612e48fa3671ea831683cc0ac7fc6c3aa7e498
Hermita11 <41049722+Hermita11@users.noreply.github.com>
2018-07-13 11:24:20 +0800

    Add files via upload

---
+++ table.php
@@ -0,0 +1,220 @@
+<form method="POST" action="http://simrs.org/demo/public" accept-charset="UTF-8" id="reg3_form" class="form-horizontal"><input name="_token" type="hidden" value="as2n3NPh8qezawnZGW8Cp8SrVeX6Fx0yCL8Ramnh">										<div class="row-fluid">
+											<div class="span8">
+												<div class="control-group">
+													<label class="control-label">Nama</label>
+													<div class="controls">
+														<input type="text" id="Nama_det" name="Nama_det" class="span12 no-primary" value="NANI, NY">
+													</div>
+												</div>
+											</div>
+											<div class="span4">
+												<div class="control-group">
+													<label class="control-label">Jenis Kelamin</label>
+													<div class="controls">
+														<input type="text" id="Jkel_det" name="Jkel_det" class="span12 no-primary" value="P">
+													</div>
+												</div>
+											</div>
+										</div>
+										<div class="row-fluid">
+											<div class="span12">
+												<div class="control-group">
+													<label class="control-label">Alamat</label>
+													<div class="controls">
+														<input type="text" id="Jalan_det" name="Jalan_det" class="span12" value="TAMBAKAN RT 26/04 0   ">
+													</div>
+												</div>
+											</div>
+										</div>
+										<div class="row-fluid">
+											<div class="span4">
+												<div class="control-group">
+													<label class="control-label">Tmp. Lahir</label>
+													<div class="controls">
+														<input type="text" id="TempatLahir_det" name="TempatLahir_det" class="span12" value="">
+													</div>
+												</div>
+												<div class="control-group">
+													<label class="control-label">Tgl. Lahir</label>
+													<div class="controls">
+														<input type="text" id="TanggalLahir_det" name="TanggalLahir_det" class="span12" value="30/11/-0001">
+													</div>
+												</div>
+												<div class="control-group">
+													<label class="control-label">Usia</label>
+													<div class="controls">
+														<input type="text" id="Usia_det" name="Usia_det" class="span12" value="6734 Th, 12 Bln, 0 Hari">
+													</div>
+												</div>
+											</div>
+											<div class="span4">							
+												<div class="control-group">
+													<label class="control-label">Status</label>
+													<div class="controls">
+														<input type="text" id="Status_det" name="Status_det" class="span12 no-primary" value="">
+													</div>
+												</div>
+												<div class="control-group">
+													<label class="control-label">Pekerjaan</label>
+													<div class="controls">
+														<input type="text" id="Pekerjaan_det" name="Pekerjaan_det" class="span12 no-primary" value="-">
+													</div>
+												</div>
+												<div class="control-group">
+													<label class="control-label">No. Telp.</label>
+													<div class="controls">
+														<input type="text" id="NoTelp_det" name="NoTelp_det" class="span12 no-primary" value="">
+													</div>
+												</div>
+											</div>
+											<div class="span4">	
+												<div class="control-group">
+													<label class="control-label">Agama</label>
+													<div class="controls">
+														<input type="text" id="Agama_det" name="Agama_det" class="span12 no-primary" value="-">
+													</div>
+												</div>
+												<div class="control-group">
+													<label class="control-label">Suku</label>
+													<div class="controls">
+														<input type="text" id="Suku_det" name="Suku_det" class="span12 no-primary" value="">
+													</div>
+												</div>
+											</div>
+										</div>
+										<hr>
+										<div class="row-fluid">
+											<div class="span4">							
+												<div class="control-group">
+													<label class="control-label">Gol. Pasien</label>
+													<div class="controls">
+														<input type="text" id="GolPasien_det" name="GolPasien_det" class="span12 no-primary" value="umum">
+													</div>
+												</div>
+												<div class="control-group">
+													<label class="control-label">Sub Gol Pasien</label>
+													<div class="controls">
+														<input type="text" id="SubGolPasien_det" name="SubGolPasien_det" class="span12 no-primary" value="-">
+													</div>
+												</div>
+												<div class="control-group">
+													<label class="control-label">No BPJS</label>
+													<div class="controls">
+														<input type="text" id="NoBPJS_det" name="NoBPJS_det" class="span12 no-primary" value="-">
+													</div>
+												</div>
+											</div>
+											<div class="span4">	
+												<div class="control-group">
+													<label class="control-label">Gol. Askes</label>
+													<div class="controls">
+														<input type="text" id="GolAskes_det" name="GolAskes_det" class="span12 no-primary" value="-">
+													</div>
+												</div>
+												<div class="control-group">
+													<label class="control-label">Gol. No. Askes</label>
+													<div class="controls">
+														<input type="text" id="GolNoAskes_det" name="GolNoAskes_det" class="span12 no-primary" value="-">
+													</div>
+												</div>
+											</div>
+											<div class="span4">	
+												<div class="control-group">
+													<label class="control-label">No Jamkesmas</label>
+													<div class="controls">
+														<input type="text" id="NoJamkesmas_det" name="NoJamkesmas_det" class="span12 no-primary" value="-">
+													</div>
+												</div>
+												<div class="control-group">
+													<label class="control-label">No Jamkesda</label>
+													<div class="controls">
+														<input type="text" id="NoJamkesda_det" name="NoJamkesda_det" class="span12 no-primary" value="-">
+													</div>
+												</div>
+											</div>
+										</div>
+										<hr>
+										<div class="row-fluid">
+											<div class="span4">							
+											   <div class="control-group">
+													<label class="control-label">Gol. Dinas</label>
+													<div class="controls">
+														<input type="text" id="GolDinas_det" name="GolDinas_det" class="span12 no-primary" value="-">
+													</div>
+												</div>
+												<div class="control-group">
+													<label class="control-label">No SEP Dinas</label>
+													<div class="controls">
+														<input type="text" id="NoSEPDinas_det" name="NoSEPDinas_det" class="span12 no-primary" value="-">
+													</div>
+												</div>
+												<div class="control-group">
+													<label class="control-label">NRPNIP</label>
+													<div class="controls">
+														<input type="text" id="NRPNIP_det" name="NRPNIP_det" class="span12 no-primary" value="-">
+													</div>
+												</div>
+											</div>
+											<div class="span4">
+												<div class="control-group">
+													<label class="control-label">Gol. Kesatuan</label>
+													<div class="controls">
+														<input type="text" id="GolKes_det" name="GolKes_det" class="span12 no-primary" value="-">
+													</div>
+												</div>
+												<div class="control-group">
+													<label class="control-label">Kesatuan</label>
+													<div class="controls">
+														<input type="text" id="Kesatuan_det" name="Kesatuan_det" class="span12 no-primary" value="-">
+													</div>
+												</div>
+												<div class="control-group">
+													<label class="control-label">Pangkat Gol.</label>
+												  <div class="controls">
+														<input type="text" id="PangkatGol_det" name="PangkatGol_det" class="span12 no-primary" value="-">
+													</div>
+												</div>
+											</div>
+											<div class="span4">
+												<div class="control-group">
+													<label class="control-label">Hubungan</label>
+													<div class="controls">
+														<input type="text" id="Hub_det" name="Hub_det" class="span12 no-primary" value="-">
+													</div>
+												</div>
+												<div class="control-group">
+													<label class="control-label">Jenis Hub.</label>
+													<div class="controls">
+														<input type="text" id="Jenishub_det" name="Jenishub_det" class="span12 no-primary" value="-">
+													</div>
+												</div>
+											</div>
+										</div>
+										<hr>
+										<div class="row-fluid">
+											<div class="span4">							
+												<div class="control-group">
+													<label class="control-label">Gol. Swasta</label>
+													<div class="controls">
+														<input type="text" id="GolSwasta_det" name="GolSwasta_det" class="span12 no-primary" value="-">
+													</div>
+												</div>
+											</div>
+											<div class="span4">	
+											   <div class="control-group">
+													<label class="control-label">No Kartu Swasta</label>
+													<div class="controls">
+														<input type="text" id="NoKartuSwasta_det" name="NoKartuSwasta_det" class="span12 no-primary" value="-">
+													</div>
+												</div>
+											</div>
+											<div class="span4">	
+												<div class="control-group">
+													<label class="control-label">Nama Perusahaan</label>
+													<div class="controls">
+														<input type="text" id="NamaPerusahaan_det" name="NamaPerusahaan_det" class="span12 no-primary" value="-">
+													</div>
+												</div>
+											</div>
+										</div>
+									</form>
