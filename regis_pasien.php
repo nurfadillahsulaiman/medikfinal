ca542aea7bb896e47740c2903cc47d466e80d09a
srihandayanias <41175716+srihandayanias@users.noreply.github.com>
2018-07-15 19:48:11 +0800

    Add files via upload

---
+++ regis_pasien.php
@@ -0,0 +1,42 @@
+<div class="container">
+  <div class="row">
+    <div class="box box-info">
+            <div class="box-header with-border">
+              <h3 class="box-title">Horizontal Form</h3>
+            </div>
+            <!-- /.box-header -->
+            <!-- form start -->
+            <form class="form-horizontal">
+              <div class="box-body">
+                <div class="form-group">
+                  <label for="tanggal" class="col-sm-2 control-label">Tanggal Masuk</label>
+
+                  <div class="col-sm-10">
+                    <input type="date" class="form-control"  placeholder="Tanggal Masuk">
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label for="time"  class="col-sm-2 control-label">Jam Masuk</label>
+
+                  <div class="col-sm-10">
+                    <input type="time" class="form-control" id="time" placeholder="Password">
+                  </div>
+                </div>
+                <label class="col-sm-2 control-label">Nama</label>
+                <div class="col-sm-6">
+                    <input type="text" class="form-control"  placeholder="Depan">
+                  </div>
+                  <div class="col-sm-4">
+                    <input type="text" class="form-control"  placeholder="Belakang">
+                  </div>
+              </div> 
+              <!-- /.box-body -->
+              <div class="box-footer">
+                <button type="submit" class="btn btn-default">Cancel</button>
+                <button type="submit" class="btn btn-info pull-right">Sign in</button>
+              </div>
+              <!-- /.box-footer -->
+            </form>
+          </div>
+  </div>
+</div>
